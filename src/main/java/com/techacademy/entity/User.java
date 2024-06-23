886bfa8e06c8460184c149d2bdbda74fe298dfb4
mtnmc <holoholo0308@gmail.com>
2024-06-23 17:12:47 +0900

    Lesson 18Chapter 7.6

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -15,30 +15,30 @@ import lombok.Data;
 @Table(name = "user")
 public class User {
 
-    /**性別用の列挙型*/
-    public static enum Gender{
+    /** 性別用の列挙型 */
+    public static enum Gender {
         男性, 女性
     }
 
-    /**主キー。自動生成*/
+    /** 主キー。自動生成 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    /**名前。20桁。null不許可 */
+    /** 名前。20桁。null不許可 */
     @Column(length = 20, nullable = false)
     private String name;
 
-    /**名前。20桁。null不許可 */
+    /** 性別。2桁。列挙型（文字列） */
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    /**年齢　*/
+    /** 年齢 */
     private Integer age;
 
-    /** メールアドレス。50桁。null不許可 */
+    /** メールアドレス。50桁。null許可 */
+    @Column(length = 50)
     private String email;
-
 
 }
