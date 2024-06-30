e43a8b9a6c5a9f28d79c3f7810787597c236f73a
mtnmc <holoholo0308@gmail.com>
2024-06-30 17:04:30 +0900

    Lesson 18Chapter 11

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PreRemove;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;// 追加
 import jakarta.validation.constraints.Max;// 追加
@@ -15,6 +17,7 @@ import jakarta.validation.constraints.NotEmpty;// 追加
 import jakarta.validation.constraints.NotNull;// 追加
 
 import org.hibernate.validator.constraints.Length;// 追加
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.Data;
 
@@ -55,5 +58,23 @@ public class User {
     @Email // 追加
     @Length(max=50)// 追加
     private String email;
+
+
+    // ----- 追加ここから -----
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    @PreRemove
+    @Transactional
+
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+
+        }
+    }
+
+    // ----- 追加ここまで -----
 
 }
